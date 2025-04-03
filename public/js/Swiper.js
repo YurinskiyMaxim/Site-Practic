1eb2e184e890b7b6ce2fe301b4a7d903550a42b0
unknown <@CHTOTIB.RU>
2025-04-03 16:55:10 +0900

    second start

---
+++ public/js/Swiper.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  breakpoints: {
+    1119: {
+      slidesPerColumn: 2,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const swiper_1 = new Swiper_1(".mySwiper_1", {
+  slidesPerView: "auto",
+  breakpoints: {
+    1490: {
+      slidesPerColumn: 2,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
